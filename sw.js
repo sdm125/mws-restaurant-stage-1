bd9d6397289224b673cd9fc4d23055aa61d8f3a3
Stacy Mackin <stacy.mackin@gmail.com>
2018-06-25 12:20:07 -0400

    fix: Add activate eventListener to sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('restaurant-reviews-v1').then(function(cache) {
+    caches.open('restaurant-reviews-v2').then(function(cache) {
       return cache.addAll([
         '/',
+        'data/restaurants.json',
+        'js/dbhelper.js',
         'js/main.js',
         'js/restaurant_info.js',
         'css/styles.css',
@@ -27,14 +29,26 @@ self.addEventListener('install', function(event) {
         'img/9_800.jpg',
         'img/10_800.jpg'
       ]);
+    }).catch(function(err){
+      return err;
     })
   );
+});
+
+self.addEventListener('activate', function(event) {
+  caches.delete('restaurant-reviews-v1').then(function(cache){
+    return cache;
+  }).catch(function(err){
+    return err;
+  })
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    }).catch(function(err){
+      return err;
     })
   );
 });
